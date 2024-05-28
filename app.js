30fdc06a768e6624baed6a9472d925b8bb32eb4b
CloneTrooper66 <luka.eli88@gmail.com>
2024-05-28 13:16:16 +0100

    solves kata 1 with test cases

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { getTopic } = require("./controllers/app.controller");
+const app = express();
+
+app.get("/api/topics", getTopic);
+
+app.use((req, res, next) => {
+  res.status(404).send({ msg: "PATH NOT FOUND" });
+  next();
+});
+
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else if (err.code === "22P02") {
+    res.status(400).send({ msg: err.message || "Bad Request" });
+  } else {
+    res.status(err.status || 500).send({ msg: "Internal Server Error" });
+  }
+});
+
+module.exports = app;
